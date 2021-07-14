d288c6a8f3116a2ef70b47623cd2e7546a835d8e
Woogie <woogie@protopie.io>
2021-07-15 00:37:32 +0900

    [#25] SubMenu - Crop 마크업
    - 기존 Canvas의 Background-color는 Black 이었는데, 기획의 변경으로 이를 모달 배경색으로 바꾸었다.

--- src/canvas/CanvasRoot.ts
+++ src/canvas/CanvasRoot.ts
@@ -3,7 +3,7 @@ import {AbstractView} from "../infra/AbstractView";
 import {LookUpElement} from "../util/LookupElement";
 import autobind from "autobind-decorator";
 import {px} from "../util/CSSUtil";
-import {Palette} from "../util/Palette";
+import {Palette} from "../util/Palette"
 
 const HTML = `
 <section class="canvas-wrapper">
@@ -49,7 +49,7 @@ export class CanvasRoot extends AbstractView {
             width: wrapperBoundingRect.width,
             height: wrapperBoundingRect.height,
         });
-        this._fillCanvasBlack();
+        this._fillBackground();
     }
     
     @autobind
@@ -60,12 +60,12 @@ export class CanvasRoot extends AbstractView {
     
     @autobind
     private _onCanvasImageChange(image: HTMLImageElement, x: number, y: number, width: number, height: number): void {
-        this._fillCanvasBlack();
+        this._fillBackground();
         this._ctx.drawImage(image, 0, 0, image.width, image.height, x, y, width, height);
     }
 
-    private _fillCanvasBlack(): void {
-        this._ctx.fillStyle = Palette.BLACK;
+    private _fillBackground(): void {
+        this._ctx.fillStyle = Palette.MODAL_BACKGROUND_COLOR;
         this._ctx.fillRect(0, 0, this._model.canvas.canvasWidth, this._model.canvas.canvasHeight);
     }
 }
