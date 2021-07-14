10bd8d802b22543291b77341c7ae334d60b1581e
Woogie <woogie@protopie.io>
2021-07-14 11:44:30 +0900

    [#24] Image Load
    - image를 불러와서 모델로 반영하는 기능

--- src/persister/PersisterRoot.ts
+++ src/persister/PersisterRoot.ts
@@ -2,6 +2,7 @@ import {AbstractView} from "../infra/AbstractView";
 import {LoadButton} from "./LoadButton";
 import {DownloadButton} from "./DownloadButton";
 import {LookUpElement} from "../util/LookupElement";
+import {ModelManager} from "../model/ModelManager";
 
 const HTML = `
 <section class="persist-area">
@@ -15,15 +16,22 @@ const HTML = `
 export class PersisterRoot extends AbstractView {
     protected htmlText: string = HTML;
     
+    private _model: ModelManager;
+    
     @LookUpElement('.buttons')
     private _buttonsEl!: HTMLDivElement;
     
     private _loadButton!: LoadButton;
     private _downloadButton!: DownloadButton;
     
+    constructor(model: ModelManager) {
+        super();
+        this._model = model;
+    }
+    
     protected onFirstRender(): void {
         super.onFirstRender();
-        this._loadButton = new LoadButton();
+        this._loadButton = new LoadButton(this._model);
         this._downloadButton = new DownloadButton();
         this._renderButtons();
     }
