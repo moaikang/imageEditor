a2b8560047dd256310f6c4411510a47f08e0902d
Woogie <woogie@protopie.io>
2021-07-14 20:33:00 +0900

    [#24] Image Load
    - 모델의 image가 변경되면 이에 따라 canvas의 image를 변경하기 이전에, 나중에 Rotate를 고려해 이미지의 크기를 조절해주는 기능 개발
    - 이미지의 대각선 길이가 캔버스의 높이보다 크다면, 대각선 길이를 캔버스의 높이로 맞춰주고, 이에 따라 가로와 세로를 변경해줌.

--- src/canvas/CanvasRoot.ts
+++ src/canvas/CanvasRoot.ts
@@ -2,12 +2,17 @@ import {ModelManager} from "../model/ModelManager";
 import {AbstractView} from "../infra/AbstractView";
 import {LookUpElement} from "../util/LookupElement";
 import autobind from "autobind-decorator";
+import {px} from "../util/CSSUtil";
+import {Palette} from "../util/Palette";
 
 const HTML = `
 <section class="canvas-wrapper">
   <canvas id="canvas"></canvas>
 </section>
 `
+
+const CANVAS_WIDTH = 1050;
+const CANVAS_HEIGHT = 370;
 
 export class CanvasRoot extends AbstractView {
     protected htmlText: string = HTML;
@@ -26,8 +31,15 @@ export class CanvasRoot extends AbstractView {
     
     protected onFirstRender() {
         super.onFirstRender();
-        this._ctx = this._canvasEl.getContext('2d')!;
+        this._initCanvas();
         this._bindEvents();
+    }
+    
+    private _initCanvas(): void {
+        this._ctx = this._canvasEl.getContext('2d')!;
+        this._canvasEl.setAttribute('width', px(CANVAS_WIDTH));
+        this._canvasEl.setAttribute('height',  px(CANVAS_HEIGHT));
+        this._fillCanvasBlack();
     }
     
     private _bindEvents(): void {
@@ -36,6 +48,28 @@ export class CanvasRoot extends AbstractView {
     
     @autobind
     private _onCanvasImageChange(image: HTMLImageElement): void {
-        this._ctx.drawImage(image, 0, 0);
+        const {width, height} = this._getImgRenderingSize(image);
+        this._fillCanvasBlack();
+        this._ctx.drawImage(image, 0, 0, image.width, image.height, (CANVAS_WIDTH - width) / 2,(CANVAS_HEIGHT - height) / 2, width, height);
+    }
+    
+    private _getImgRenderingSize(image: HTMLImageElement): {width: number, height: number} {
+        const {width, height} = image;
+        const diagonal = Math.ceil(Math.sqrt(width * width + height * height));
+        let renderingWidth = width;
+        let renderingHeight = height;
+    
+        if (diagonal > CANVAS_HEIGHT) {
+            const ratio = CANVAS_HEIGHT / diagonal;
+            renderingWidth = width * ratio;
+            renderingHeight = height * ratio;
+        }
+        
+        return {width: renderingWidth, height: renderingHeight};
+    }
+    
+    private _fillCanvasBlack(): void {
+        this._ctx.fillStyle = Palette.BLACK;
+        this._ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 }
