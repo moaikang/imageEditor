f7f1ee6df78c9dea7632ff6848ddb2f45a6fe9ea
Woogie <woogie@protopie.io>
2021-07-25 04:05:00 +0900

    [#35] 1차 리팩토링
    - SCSS 리팩토링
    - Responsive 적용

--- src/util/CommandApiDecorator.ts
+++ src/util/CommandApiDecorator.ts
@@ -7,17 +7,15 @@ export function CommandApi(commandName: CommandName) {
         const className = (constructorFn as any).name;
         if (commandNameMap.has(className)) {
             throw new Error('');
-        } else {
-            commandNameMap.set(className, commandName);
         }
+        commandNameMap.set(className, commandName);
     }
 }
 
 export function initWithAssignCommandName(target: object): string {
     const className = target.constructor.name;
-    if (commandNameMap.has(className)) {
-        return commandNameMap.get(className)!;
-    } else {
+    if (!commandNameMap.has(className)) {
         throw new Error('');
     }
+    return commandNameMap.get(className)!;
 }
